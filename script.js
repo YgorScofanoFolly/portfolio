e17a177f0f967bad3630e0ac01ca78f30ecdd6e4
Ygor Scofano <97801649+YgorScofanoFolly@users.noreply.github.com>
2022-11-03 15:01:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let sections = document.querySelectorAll('.animate');
-function activeDisplay() {
-    sections.forEach(element => {
-        element.classList.add('active');
+window.onload = function() {
+    let animateElements = document.querySelectorAll('.js-animate');
+    function ActiveDisplay() {
+        this.show = function() {
+            animateElements.forEach(e => {
+                e.classList.add('activeAnimate');
+            });
+        };
+    };
+    let showElements = new ActiveDisplay();
+    showElements.show();
+    let clickEvent = document.querySelectorAll('.clickEvent');
+    let arrows = document.querySelectorAll('.arrow');
+    function ShowInfoText() {
+        this.show = function(i) {
+            clickEvent[i].nextElementSibling.classList.toggle('showInfo');
+            arrows[i].classList.toggle('rotate');
+        };
+    };
+    let arrowEvent = new ShowInfoText();
+    clickEvent.forEach((e, i) => {
+        e.addEventListener('click', function() {
+            arrowEvent.show(i);
+        });
     });
 };
-window.addEventListener('load', activeDisplay);
